60b8ca9c04c6ae61eac0aebf9b3dd586c54deed6
syrupythires <SyrupyThires@gmail.com>
2025-12-27 02:29:46 -0700

    Initial commit

---
+++ src/main/java/net/syrupythires/soultorn/SoultornClient.java
@@ -0,0 +1,10 @@
+package net.syrupythires.soultorn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoultornClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
