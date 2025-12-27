c06638a4b0ac829f64218ec5f226240c62c7c0ba
syrupythires <SyrupyThires@gmail.com>
2025-12-27 02:44:37 -0700

    added items, im going to bed

---
+++ src/main/java/net/syrupythires/soultorn/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.syrupythires.soultorn.item;
+
+import net.syrupythires.soultorn.Soultorn;
+
+public class ModItems {
+
+    public static void registerModItems() {
+        Soultorn.LOGGER.info("Registering Mod Items for " + Soultorn.MOD_ID);
+    }
+}
